9c65e4f1ddfa2f9a7b735d6d65becc320dafe174
Dominique B <domi_burnand@yahoo.de>
2015-02-15 17:32:48 +0100

    [Cleanup] examples: rewrite controlled-browser with kv-lang. Add a minimal browser-view example.

---
+++ examples/minimal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+Minimal example of the CEFBrowser widget use. Here you don't have any controls
+(back / forth / reload) or whatsoever. Just a kivy app displaying the
+chromium-webview.
+"""
+
+
+from kivy.app import App
+from kivy.garden.cefpython import CEFBrowser
+
+
+if __name__ == '__main__':
+    class SimpleBrowserApp(App):
+        def build(self):
+            return CEFBrowser(url="http://www.rentouch.ch")
+
+    SimpleBrowserApp().run()
+    cefpython.Shutdown()
+
