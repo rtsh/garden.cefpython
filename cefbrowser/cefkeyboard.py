rtsh/garden.cefpython

commit f8916c73f36dd7ef79d5967e251f4ef07134b0ac

[Fix] cefkeyboard: Tab (did not jump from field to field) and Enter key did not work in browser-view. Send rawkey now at every key-down. Add special handling for enter (keycode 13)

cefbrowser/cefkeyboard.py
@@ -43,7 +43,7 @@ class CEFKeyboardManagerSingleton():
         self.process_key_down(browser, None, keycode, text, modifiers)
     
     def kivy_on_key_down(self, browser, keyboard, keycode, text, modifiers):
-        whitelist = (8, 27)
+        whitelist = (9, 8, 13, 27)
         if Window.__class__.__module__ == 'kivy.core.window.window_sdl2' and \
                 (keycode[0] not in whitelist):
             return
@@ -75,6 +75,10 @@ class CEFKeyboardManagerSingleton():
                 "__kivy__on_escape()")
             return
 
+        # Modify text for enter key
+        if key[0] == 13:
+            text = "\r"
+
         # CEF modifiers
         # When pressing ctrl also set modifiers for ctrl
         if key[0] in (306, 305):
@@ -94,6 +98,17 @@ class CEFKeyboardManagerSingleton():
         if text:
             charcode = ord(text)
 
+        # Send key event to cef: RAWKEYDOWN
+        keyEvent = {
+                "type": cefpython.KEYEVENT_RAWKEYDOWN,
+                "windows_key_code": keycode,
+                "character": charcode,
+                "unmodified_character": charcode,
+                "modifiers": cef_modifiers,
+        }
+        # print("- DOWN SendKeyEvent: %s" % keyEvent)
+        browser.SendKeyEvent(keyEvent)
+
         # Send key event to cef: CHAR
         if text:
             keyEvent = {
@@ -103,18 +118,7 @@ class CEFKeyboardManagerSingleton():
                     "unmodified_character": charcode,
                     "modifiers": cef_modifiers
             }
-            # print("- SendKeyEvent: %s" % keyEvent)
-            browser.SendKeyEvent(keyEvent)
-        else:
-            # Send key event to cef: RAWKEYDOWN
-            keyEvent = {
-                    "type": cefpython.KEYEVENT_RAWKEYDOWN,
-                    "windows_key_code": keycode,
-                    "character": charcode,
-                    "unmodified_character": charcode,
-                    "modifiers": cef_modifiers,
-            }
-            # print("- SendKeyEvent: %s" % keyEvent)
+            # print("- DOWN text SendKeyEvent: %s" % keyEvent)
             browser.SendKeyEvent(keyEvent)
 
         if key[0] == 304:
@@ -159,7 +163,7 @@ class CEFKeyboardManagerSingleton():
                 "unmodified_character": charcode,
                 "modifiers": cef_modifiers
         }
-        # print("- SendKeyEvent: %s" % keyEvent)
+        # print("- UP SendKeyEvent: %s" % keyEvent)
         browser.SendKeyEvent(keyEvent)
 
         if key[0] == 304:

cefbrowser/version.py
@@ -2,4 +2,4 @@
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
-__version__ = '0.5.40'
+__version__ = '0.5.42'

cefbrowser/cefbrowser.py
@@ -394,11 +394,11 @@ class CEFBrowser(Widget, FocusBehavior):
         return False
 
     def keyboard_on_key_down(self, *largs):
-        #print("KEY DOWN", largs)
+        # print("KEY DOWN", largs)
         CEFKeyboardManager.kivy_on_key_down(self._browser, *largs)
 
     def keyboard_on_key_up(self, *largs):
-        #print("KEY UP", largs)
+        # print("KEY UP", largs)
         CEFKeyboardManager.kivy_on_key_up(self._browser, *largs)
 
     def keyboard_on_textinput(self, window, text):

commit b05d3a86a7f48f554b5a9fd3399d2320b02a65f3

[Fix] special keys (keycode 35-40) aka ($#%& This bug was re-introduced with f8916c73f36dd7ef79d5967e251f4ef07134b0ac

cefbrowser/cefkeyboard.py
@@ -98,16 +98,18 @@ class CEFKeyboardManagerSingleton():
         if text:
             charcode = ord(text)
 
-        # Send key event to cef: RAWKEYDOWN
-        keyEvent = {
-                "type": cefpython.KEYEVENT_RAWKEYDOWN,
-                "windows_key_code": keycode,
-                "character": charcode,
-                "unmodified_character": charcode,
-                "modifiers": cef_modifiers,
-        }
-        # print("- DOWN SendKeyEvent: %s" % keyEvent)
-        browser.SendKeyEvent(keyEvent)
+        # Do not send RAW-key for key-codes 35-40 aka ($#%&
+        if key[0] not in range(35, 40):
+            # Send key event to cef: RAWKEYDOWN
+            keyEvent = {
+                    "type": cefpython.KEYEVENT_RAWKEYDOWN,
+                    "windows_key_code": keycode,
+                    "character": charcode,
+                    "unmodified_character": charcode,
+                    "modifiers": cef_modifiers,
+            }
+            # print("- DOWN RAW SendKeyEvent: %s" % keyEvent)
+            browser.SendKeyEvent(keyEvent)
 
         # Send key event to cef: CHAR
         if text:

cefbrowser/version.py
@@ -2,4 +2,4 @@
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
-__version__ = '0.5.52'
+__version__ = '0.5.53'